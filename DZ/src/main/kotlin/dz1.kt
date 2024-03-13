93db1bfb4169cd9e23c2914bf69f212712ee88f8
Lidiya <lesnikoval@yandex.ru>
2024-03-13 14:26:37 +0300

    dz complete

---
+++ DZ/src/main/kotlin/dz1.kt
@@ -0,0 +1,33 @@
+/*Написать программу, которая обрабатывает введённые пользователем в консоль команды:
+exit
+help
+add <Имя> phone <Номер телефона>
+add <Имя> email <Адрес электронной почты>
+После выполнения команды, кроме команды exit, программа ждёт следующую команду.*/
+
+fun main() {
+    println("Enter one of the commands:\n" +
+            " exit\n" +
+            " help\n" +
+            " add <Имя> phone <Номер телефона>\n" +
+            " add <Имя> email <Адрес электронной почты>\n")
+    val answer = readlnOrNull().toString();
+    when (answer) {
+        "exit" -> println("Goodbye")
+        "help" -> {
+            println("Help output\n")
+            main()}
+        else -> {
+            answerSplit(answer.split(" "))
+            main()}
+    }
+}
+
+fun answerSplit(args: List<String>) {
+    if ((args.size == 4) && (args[0] == "add") && (args[2] == "phone" || args[2] == "email"))
+        if ((args[2]=="phone" && args[3].matches(Regex("""\+[0-9]+"""))) ||
+            (args[2]=="email" && args[3].matches(Regex("""\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6}"""))))
+            println("Contact information ${args[1]}:  ${args[2]} ${args[3]}\n")
+        else println("Incorrect contact information format")
+    else println("Command input error, try again")
+}
