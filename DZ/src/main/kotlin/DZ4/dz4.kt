7c1d0bf18b184dabdf68eef888d75e033850dde0
Lidiya <lesnikoval@yandex.ru>
2024-03-18 21:33:14 +0300

    dz4 complete

--- DZ/src/main/kotlin/DZ4/dz4.kt
+++ DZ/src/main/kotlin/DZ4/dz4.kt
@@ -1,12 +1,15 @@
-package DZ3
+package DZ4
+
+import java.io.File
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 
 /*
-  — Измените класс Person так, чтобы он содержал список телефонов и список почтовых адресов, связанных с человеком.
-— Теперь в телефонной книге могут храниться записи о нескольких людях. Используйте для этого наиболее подходящую структуру данных.
-— Команда AddPhone теперь должна добавлять новый телефон к записи соответствующего человека.
-— Команда AddEmail теперь должна добавлять новый email к записи соответствующего человека.
-— Команда show должна принимать в качестве аргумента имя человека и выводить связанные с ним телефоны и адреса электронной почты.
-— Добавьте команду find, которая принимает email или телефон и выводит список людей, для которых записано такое значение.
+— Добавьте новую команду export, которая экспортирует добавленные значения в текстовый файл в формате JSON.
+Команда принимает путь к новому файлу. Например export /Users/user/myfile.json
+— Реализуйте DSL на Kotlin, который позволит конструировать JSON и преобразовывать его в строку.
+— Используйте этот DSL для экспорта данных в файл.
+— Выходной JSON необязательно должен быть отформатирован, поля объектов могут идти в любом порядке.
+Главное, чтобы он имел корректный синтаксис.
 */
 sealed interface Command {
     fun isValid(): Boolean
@@ -36,8 +39,16 @@ class Find(val args: List<String>) : Command {
     override fun isValid(): Boolean = (args.size == 2)
 }
 
+class Export(val args: List<String>) : Command {
+    override fun isValid(): Boolean = (args.size == 2 && !args[1].isEmpty())
+}
+
 data class Person(var name: String? = null, var phone: String? = null, var email: String? = null) {
     var listPerson = mutableMapOf(name to arrayOf(mutableListOf(phone), mutableListOf(email)))
+
+    init {
+        listPerson.remove(key = null)
+    }
 
     fun getContactInfomation(key: String): Array<MutableList<String?>>? {
         return listPerson[key]
@@ -71,6 +82,11 @@ fun readCommand(args: List<String>): Command {
             println("Command input error, try again")
             Help()
         }
+        "EXPORT" -> if (Export(args).isValid()) Export(args)
+        else {
+            println("Command input error, try again")
+            Help()
+        }
         else -> {
             println("Command input error, try again")
             Help()
@@ -78,9 +94,9 @@ fun readCommand(args: List<String>): Command {
     }
 }
 
+var person = Person()
 fun main() {
     var flag = true
-    var person: Person = Person()
 
     while (flag) {
         println(
@@ -90,6 +106,7 @@ fun main() {
                     " add <Имя> email <Адрес электронной почты>\n" +
                     " show <Имя> (or <ALL>)\n" +
                     " find <строка поиска>\n" +
+                    " export <путь к новому файлу> (or './' текущий каталог)\n" +
                     " exit\n"
         )
         val answer = readlnOrNull().toString().uppercase().split(" ");
@@ -100,45 +117,59 @@ fun main() {
                 flag = false
             }
             is Help -> println("Help output\n")
-            is Show -> {
-                if (answer[1] == "ALL") person.toString()
-                else if (person.getContactInfomation(answer[1]) == null) println("Not initialized\n")
-                else println("Contact information ${answer[1]}:\n" +
-                            "PHONE - ${person.getContactInfomation(answer[1])!![0]}\n" +
-                            "EMAIL - ${person.getContactInfomation(answer[1])!![1]}\n")
-            }
-            is Add -> {
-                if (person.getContactInfomation(answer[1])!=null) {
-                    var phone = person.getContactInfomation(answer[1])!!.get(0)
-                    var email = person.getContactInfomation(answer[1])!!.get(1)
-                    if (answer[2] == "PHONE") {
-                        if (phone.get(0) == null) phone.set(0, answer[3])
-                        else phone.add(answer[3])
-
-                    } else if (answer[2] == "EMAIL") {
-                        if (email.get(0) == null) email.set(0, answer[3])
-                        else email.add(answer[3])
-                    }
-                    val contactInfomation = arrayOf(phone,email)
-                    person.setListPerson(answer[1], contactInfomation)
-                } else {
-                    var phone: String? = null
-                    var email: String? = null
-                    if (answer[2] == "PHONE") {
-                        phone = answer[3]
-                    } else if (answer[2] == "EMAIL") {
-                        email = answer[3]
-                    }
-                    person.setListPerson(answer[1], arrayOf(mutableListOf(phone), mutableListOf(email)))
-                }
-                println("Contact information added\n")
-            }
-            is Find -> {
-                val filtrperson = person.listPerson.filter {person -> person.value[0].filterNotNull().filter { contact -> contact.contains(answer[1]) }.count()>0 ||
-                        person.value[1].filterNotNull().filter { contact -> contact.contains(answer[1]) }.count()>0}
-                if (filtrperson.isEmpty()) println("No records found\n")
-                else filtrperson.forEach { t, u -> println("name: "+t.toString()+", phone: "+ u[0].toString()+", email: "+ u[1].toString())}
-            }
+            is Show -> showContactInformation(answer)
+            is Add  -> addContact(answer)
+            is Find -> findContact(answer)
+            is Export -> exportListContact(answer)
         }
     }
+}
+
+fun showContactInformation(answer: List<String>) {
+    if (answer[1] == "ALL") person.toString()
+    else if (person.getContactInfomation(answer[1]) == null) println("Not initialized\n")
+    else println("Contact information ${answer[1]}:\n" +
+            "PHONE - ${person.getContactInfomation(answer[1])!![0]}\n" +
+            "EMAIL - ${person.getContactInfomation(answer[1])!![1]}\n")
+}
+
+fun addContact(answer: List<String>) {
+    if (person.getContactInfomation(answer[1])!=null) {
+        var phone = person.getContactInfomation(answer[1])!!.get(0)
+        var email = person.getContactInfomation(answer[1])!!.get(1)
+        if (answer[2] == "PHONE") {
+            if (phone.get(0) == null) phone.set(0, answer[3])
+            else phone.add(answer[3])
+
+        } else if (answer[2] == "EMAIL") {
+            if (email.get(0) == null) email.set(0, answer[3])
+            else email.add(answer[3])
+        }
+        val contactInfomation = arrayOf(phone,email)
+        person.setListPerson(answer[1], contactInfomation)
+    } else {
+        var phone: String? = null
+        var email: String? = null
+        if (answer[2] == "PHONE") {
+            phone = answer[3]
+        } else if (answer[2] == "EMAIL") {
+            email = answer[3]
+        }
+        person.setListPerson(answer[1], arrayOf(mutableListOf(phone), mutableListOf(email)))
+    }
+    println("Contact information added\n")
+}
+
+fun findContact(answer: List<String>) {
+    val filtrPerson = person.listPerson.filter {person -> person.value[0].filterNotNull().filter { contact -> contact.contains(answer[1]) }.count()>0 ||
+            person.value[1].filterNotNull().filter { contact -> contact.contains(answer[1]) }.count()>0}
+    if (filtrPerson.isEmpty()) println("No records found\n")
+    else filtrPerson.forEach { t, u -> println("name: "+t.toString()+", phone: "+ u[0].toString()+", email: "+ u[1].toString())}
+}
+
+fun exportListContact(answer: List<String>) {
+    val mapper = jacksonObjectMapper()
+    val jsonArray = mapper.writeValueAsString(person.listPerson)
+    File(answer[1], "ContactInformation.json").writeText(jsonArray)
+    println("The data is saved to a file ${answer[1]}ContactInformation.json:\n$jsonArray\n")
 }
